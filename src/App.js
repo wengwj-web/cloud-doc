ffd5d60e3ac93f6a7cf83440727c8b7a9f3b6ed1
wengwj-web <386010929@qq.com>
2020-01-05 21:28:49 +0800

    项目构建+完成输入框部分

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import FileSearch from './components/FileSearch'
+
+function App() {
+  return (
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col bg-light left-panel">
+          <FileSearch title='我的云文档' onFileSearch={(value) => {console.log(value) }} />
+        </div>
+        <div className="col bg-primary right-panel">
+          <h1>this is right</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
